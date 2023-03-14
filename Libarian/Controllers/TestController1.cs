e412f908bdce1f9e9e23bcc7ae660e16286bcc74
tranviendai <viendai50@gmail.com>
2023-03-14 22:05:56 +0700

    Alo aaaaaaUpdate Database 03

---
+++ Libarian/Controllers/TestController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Librarian.Controllers
+{
+    public class TestController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
