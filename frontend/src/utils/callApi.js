73a3a3f9d2722c5ee7089bfacc6ac087f58e5d07
H-a-n-k <haunguyen231122@gmail.com>
2023-04-26 11:00:23 +0700

    api url

--- frontend/src/utils/callApi.js
+++ frontend/src/utils/callApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-//const baseURL = 'http://10.21.18.181/api';
+//const baseURL = 'http://10.21.16.49/api';
 const baseURL = 'http://localhost:5137/api';
 
 const CallApi = axios.create({
